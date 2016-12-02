4fe1a89b19f80cefca5beb0421f1c4fb0dfbcc42
Rico Herlt <rherlt@h3ko.de>
2016-12-02 12:40:16 +0100

    added SimpleGet Task

---
+++ 01-SimpleGet.js
@@ -0,0 +1,30 @@
+var http = require('http');
+var url = require('url');
+
+http.createServer(function (request, response) {
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+    var queryData = url.parse(request.url, true).query;
+    var firstname =  queryData.fn;
+    var lastname =  queryData.ln;
+
+   if(request.method=='GET') 
+   {
+       if (firstname && lastname)
+       {
+           response.end('Hello '+firstname+' '+lastname+'. This was an awesome request with query arguments!');
+       }
+       else
+        response.end('This was an awesome request, but unfortunately there are some missing query arguments'); 
+   } else
+   {       
+        response.end('Wow, this was an awesome '+request.method+' request. But you were requestesd to make a GET request with query arguments!\n'); 
+   }
+   
+}).listen(8081);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
