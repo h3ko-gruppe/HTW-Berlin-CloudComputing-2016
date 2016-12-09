e3002c3269750655dee39db28bd00ec4e5d046ef
Rico Herlt <rherlt@h3ko.de>
2016-12-09 10:38:39 +0100

    changed server to tutorial

--- server.js
+++ server.js
@@ -1,22 +1,14 @@
-var http = require('http');
-var port = process.env.port | 1337;
+var express = require('express');
+var app = express();
+var port = process.env.port || 1337
 
-http.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   if(request.method=='POST') {
-       // Send the response body as "Hello Cloud"
-        response.end('Wow, this was an awesome POST request!\n'); 
-   }
-   else
-   {
-        // Send the response body as "Hello Cloud"
-        response.end('Hello Cloud\n'); 
-   } 
-}).listen(port);
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
 
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+var server = app.listen(port, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
