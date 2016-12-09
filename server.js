7310c0353ec2beb4aaf7884bbaf8471c4ee9a9bc
Rico Herlt <rherlt@h3ko.de>
2016-12-09 12:05:58 +0100

    try another sample

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var http = require('http');
+var url = require('url');
 
 http.createServer(function (request, response) {
    // Send the HTTP header 
@@ -6,14 +7,28 @@ http.createServer(function (request, response) {
    // Content Type: text/plain
    response.writeHead(200, {'Content-Type': 'text/plain'});
    
-   if(request.method=='POST') {
-       // Send the response body as "Hello Cloud"
-        response.end('Wow, this was an awesome POST request!\n'); 
-   }
-   else
-   {
-        // Send the response body as "Hello Cloud"
-        response.end('Hello Cloud\n'); 
-   } 
-}).listen(process.env.PORT);
+    var queryData = url.parse(request.url, true).query;
+    var firstname =  queryData.fn;
+    var lastname =  queryData.ln;
 
+   if(request.method=='POST') 
+   {
+        var body = [];
+        request.on('data', function(chunk) {
+            body.push(chunk);
+        }).on('end', function() {
+            body = Buffer.concat(body).toString();
+                // at this point, `body` has the entire request body stored in it as a string
+                response.end('Hi '+firstname+' '+lastname+'. This was an awesome request with query arguments! This is your provided e-mail address: '+body);
+       
+            });       
+    }
+    else
+   {       
+        response.end('Wow, this was an awesome '+request.method+' request. But you were requestesd to make a POST request.!\n'); 
+   }
+   
+}).listen(8081);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
