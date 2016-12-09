0f84b8fffa4ecb10f22adc13ecfb408f9960cc71
Rico Herlt <rherlt@h3ko.de>
2016-12-09 10:49:29 +0100

    another try

--- server.js
+++ server.js
@@ -1,14 +1,6 @@
-var express = require('express');
-var app = express();
-var port = process.env.port || 1337
+var http = require('http');
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
-
-var server = app.listen(port, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log('Example app listening at http://%s:%s', host, port);
-});
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello, world! [helloworld sample]');
+}).listen(process.env.PORT);  
